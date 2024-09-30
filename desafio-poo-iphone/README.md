b74c25c988e760a6cd34bcdcbae5a348c3e9aa33
RicardoGraziato <ricardograziato@gmail.com>
2024-09-30 18:30:26 -0300

    Desafio básico de noções em POO - iPhone básico

---
+++ desafio-poo-iphone/README.md
@@ -0,0 +1,7 @@
+## Enunciado:
+
+Chegou a hora de colocar em prática a análise de negócios, requisitos, conceitos de programação orientada a objetos e modelagem UML. Com base no vídeo de lançamento do iPhone, utilize uma ferramenta de UML de sua preferência para elaborar a diagramação das classes e interfaces, representando as funções do iPhone como Reprodutor Musical, Aparelho Telefônico e Navegador na Internet. Após isso, crie as classes e interfaces no formato de arquivos .java
+
+## Diagrama UML:
+
+![Diagrama UML](diagrama-uml.jpg)
---
+++ desafio-poo-iphone/src/iPhone.java
@@ -0,0 +1,28 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() { /* tocando */ }
+
+    @Override
+    public void pausar() { /* pause */ }
+
+    @Override
+    public void selecionarMusica(String musica) { /* musica selecionada */ }
+
+    @Override
+    public void ligar(String numero) { /* ligando */ }
+
+    @Override
+    public void atender() { /* atendendo */ }
+
+    @Override
+    public void iniciarCorreioVoz() { /* correio de voz */ }
+
+    @Override
+    public void exibirPagina(String url) { /* exibindo pagina */ }
+
+    @Override
+    public void adicionarNovaAba() { /* nova aba */ }
+
+    @Override
+    public void atualizarPagina() { /* atualizando */ }
+}
